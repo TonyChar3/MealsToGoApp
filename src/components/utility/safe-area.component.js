6825a870e2b68b38727911a8604d7d6d0e5cb4c9
Tony Char3 <tonyc6731@gmail.com>
2024-01-16 14:41:22 -0500

    UPDATE: New feature added

---
+++ src/components/utility/safe-area.component.js
@@ -0,0 +1,9 @@
+import { StatusBar, SafeAreaView, Platform } from "react-native";
+import styled from "styled-components/native";
+
+const statusBarHeight = Platform.OS === "android" ? StatusBar.currentHeight : 0;
+
+export const SafeArea = styled(SafeAreaView)`
+  flex: 1;
+  margin-top: ${statusBarHeight}px;
+`;
